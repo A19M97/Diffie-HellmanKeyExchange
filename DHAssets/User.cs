0d1793632eaffb21f9b33ba02ffd1f07e983a6d1
Andrea Mugnai <andrea.mugnai97@gmail.com>
2021-01-11 12:04:57 +0100

    Working in user class. Constructor added.

--- DHAssets/User.cs
+++ DHAssets/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,27 @@ namespace DHAssets
 {
     public class User
     {
+        private long Base;
+        private long Q;
+        private long RandomNumber;
+        private long OtherUserNumber;
+        private long Key;
+
+        public User(long _base, long q)
+        {
+            Base = _base;
+            Q = q;
+            RandomNumber = ChooseRandomNumber();
+        }
+
+        public void KeyExchange(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        private long ChooseRandomNumber()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
