d6b1499210d9c539ff0bcbc905a5b54a0d146b3f
Andrea Mugnai <andrea.mugnai97@gmail.com>
2021-01-12 10:51:38 +0100

    Removed user class and Client and Server added. KeyExchange in Client class implemented.

---
+++ DHAssets/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHAssets
+{
+    public class Client
+    {
+        private readonly long _alfa;
+        private readonly long _q;
+        private double _xa;
+        private double _yb;
+        private double _key;
+
+        public Client(long alfa, long q)
+        {
+            _alfa = alfa;
+            _q = q;
+        }
+
+        public void KeyExchange(Server server)
+        {
+            _xa = ChooseRandomNumber();
+            var intermediateValue = CalculateIntermediateValue();
+            server.ReceiveFromClient(intermediateValue);
+            _yb = server.sendYb();
+            _key = Math.Pow(_yb, _xa);
+
+            double CalculateIntermediateValue() => Math.Pow(_alfa, _xa) % _q;
+
+            double ChooseRandomNumber()
+            {
+                var rand = new Random();
+                return rand.Next();
+            }
+        }
+
+    }
+}
---
+++ DHAssets/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHAssets
+{
+    public class Server
+    {
+        public void ReceiveFromClient(double intermediateValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long sendYb()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
